b7602bdacb2570752a62e41c4362e8d844a447f7
Ilyas RIMA <irilias@gmail.com>
2025-02-04 23:28:34 +0100

    setting up host - config & loggin

--- src/Robot/Robot.Application/Program.cs
+++ src/Robot/Robot.Application/Program.cs
@@ -1,6 +1,30 @@
-﻿namespace Robot.Application;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace Robot.Application;
 
 internal static class Program
 {
-    private static void Main(string[] args) => Console.WriteLine($"Hello, World! {args}");
+    private static void Main(string[] args)
+    {
+        using IHost host = CreateHostBuilder(args).Build();
+    }
+    private static IHostBuilder CreateHostBuilder(string[] args) =>
+       Host.CreateDefaultBuilder(args)
+       .ConfigureAppConfiguration((hostingContext, config) =>
+       {
+           config.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+           config.AddEnvironmentVariables();
+           config.AddCommandLine(args);
+       })
+       .ConfigureServices((context, services) =>
+       {
+          
+       })
+       .ConfigureLogging(logging =>
+       {
+           logging.ClearProviders();
+           logging.AddConsole();
+       });
 }
